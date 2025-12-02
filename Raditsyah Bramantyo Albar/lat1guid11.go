a3e6db88edf9e500e517750543e1774837354cc5
RaditsyahBA12 <rbramantyoalbar@gmail.com>
2025-12-03 01:23:30 +0700

    Add files via upload

---
+++ Raditsyah Bramantyo Albar/lat1guid11.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var pH float64
+
+	fmt.Print("Masukan PH air: ")
+	fmt.Scan(&pH)
+
+	switch {
+	case pH < 0 || pH > 14:
+		fmt.Println("Nilai pH tidak valid. Nilai pH harus antara 0 dan 14")
+	case pH >= 6.5 && pH <= 8.6:
+		fmt.Println("Air layak minum")
+
+	default:
+		fmt.Println("Air tidak layak minum")
+	}
+}
